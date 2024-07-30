24e9b031bf7b64666eb30c537ae18a48e6aceb2f
Mirror83 <glenochieng045@gmail.com>
2024-07-31 00:50:53 +0300

    Added app colour scheme

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,16 @@
 export default {
   content: ["./src/**/*.{html,js,svelte,ts}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "strong-cyan": "hsl(172 67% 45% / <alpha-value>)",
+        "very-dark-cyan": "hsl(183 100% 15% / <alpha-value>)",
+        "dark-cyan": "hsl(186 14% 43% / <alpha-value>)",
+        "grayish-cyan": "hsl(184 14% 56% / <alpha-value>)",
+        "light-cyan": "hsl(185 41% 84% / <alpha-value>)",
+        "very-light-cyan": "hsl(189 41% 97% / <alpha-value>)",
+      },
+    },
   },
   plugins: [],
 };
