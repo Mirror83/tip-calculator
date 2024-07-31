39cc6473aac6bbe47d58ec049120f5ab80745cf2
Mirror83 <glenochieng045@gmail.com>
2024-07-31 10:30:46 +0300

    Replaced trucate with format cost

--- src/lib/utils/calculate-tip.test.ts
+++ src/lib/utils/calculate-tip.test.ts
@@ -2,16 +2,11 @@ import { expect, test } from "vitest";
 import {
   calculateTipPerPerson,
   calculateTotalPerPerson,
-  truncate,
+  formatCost,
 } from "./calculate-tip";
 
-test("truncate", () => {
-  expect(truncate(3.14159)).toBe(3.14);
-  expect(truncate(3.14159, 3)).toBe(3.141);
-});
-
 test("calculateTipPerPerson", () => {
-  expect(truncate(calculateTipPerPerson(142.55, 0.15, 5))).toBe(4.27);
+  expect(formatCost(calculateTipPerPerson(142.55, 0.15, 5))).toBe("$4.27");
   expect(() => calculateTipPerPerson(100, 20, 2)).toThrowError(
     /Percentage must be between 0 and 1/
   );
