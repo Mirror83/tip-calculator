4d931d21cf78a299da19cca63a28e7bd8dfdfcaa
Mirror83 <glenochieng045@gmail.com>
2024-08-04 21:16:33 +0300

    Added flex-wrap and different text size on large screens for cost text

--- src/lib/components/ResultRow.svelte
+++ src/lib/components/ResultRow.svelte
@@ -3,10 +3,10 @@
   export let cost;
 </script>
 
-<div class="flex justify-between items-center mb-8">
+<div class="flex flex-wrap gap-4 justify-between items-end mb-8">
   <div class="flex flex-col">
     <span class="text-white">{title}</span>
     <span class="text-grayish-cyan text-sm">/ person</span>
   </div>
-  <span class="text-3xl text-strong-cyan">{cost}</span>
+  <span class="text-3xl lg:text-4xl text-strong-cyan">{cost}</span>
 </div>
