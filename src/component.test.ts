e5d740ef7fc8f2dd9aa18b47316581a1bd47ed7e
Mirror83 <glenochieng045@gmail.com>
2024-09-29 23:46:12 +0300

    chore: changed sample test

--- src/component.test.ts
+++ src/component.test.ts
@@ -1,10 +1,9 @@
 import { render, screen } from "@testing-library/svelte";
 import App from "../src/App.svelte";
-import { describe, it, expect } from "vitest";
 
 describe("App", () => {
   it("renders", () => {
     render(App);
-    expect(screen.queryByText("$0.00")).toBeVisible();
+    expect(screen.queryByText("Bill")).toBeVisible();
   });
 });
