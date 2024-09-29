b5d2355c1c845b4141aa3df5a227c881cbcd31ab
Mirror83 <glenochieng045@gmail.com>
2024-09-29 23:38:36 +0300

    fix: fixed setup for svelte testing library
    the main issue was that the types for @testing-library/svelte were not included in the compilerOptions, thus VSCode did not recognize the methods from the testing library

--- vite.config.ts
+++ vite.config.ts
@@ -8,9 +8,4 @@ import { svelteTesting } from "@testing-library/svelte/vite";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [svelte(), svelteTesting()],
-  test: {
-    environment: "jsdom",
-    globals: true,
-    setupFiles: ["./vitest-setup.js"],
-  },
 });
