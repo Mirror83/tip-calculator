c3f390a27a9fde2d9cd67af68f8847d0cafde722
Mirror83 <glenochieng045@gmail.com>
2024-12-12 10:31:07 +0300

    chore: added test for percentage buttons

--- src/App.test.ts
+++ src/App.test.ts
@@ -7,6 +7,50 @@ describe("App", () => {
     // @ts-ignore
     render(App);
   });
+
+  it("updates tip and total amount per person when tip percentage buttons are pressed given bill and people inputs are correct", async () => {
+    // @ts-ignore
+    render(App);
+
+    const billInput: HTMLInputElement = screen.getByLabelText("Bill");
+    const peopleInput: HTMLInputElement =
+      screen.getByLabelText("Number of people");
+
+    // The expected button percentages are 5%, 10%, 15%, 25%, 50%
+    const tipPercentageButtons: HTMLButtonElement[] =
+      screen.getAllByText(/\d{1,2}%$/);
+
+    const tipAmountPerPerson: HTMLElement =
+      screen.getByTestId("tip-per-person");
+
+    const totalAmountPerPerson: HTMLElement =
+      screen.getByTestId("total-per-person");
+
+    expect(tipAmountPerPerson).toHaveTextContent("$0.00");
+    expect(totalAmountPerPerson).toHaveTextContent("$0.00");
+
+    expect(tipPercentageButtons).toHaveLength(5);
+
+    await userEvent.type(billInput, "100");
+    await userEvent.type(peopleInput, "2");
+
+    let currentTipAmountPerPerson = "$0.00";
+    let currentTotalAmountPerPerson = "$0.00";
+
+    for (const button of tipPercentageButtons) {
+      await userEvent.click(button);
+      expect(button).toHaveClass("bg-strong-cyan");
+      expect(tipAmountPerPerson).not.toHaveTextContent(
+        currentTipAmountPerPerson
+      );
+      expect(totalAmountPerPerson).not.toHaveTextContent(
+        currentTotalAmountPerPerson
+      );
+      currentTipAmountPerPerson = tipAmountPerPerson.textContent!;
+      currentTotalAmountPerPerson = totalAmountPerPerson.textContent!;
+    }
+  });
+
   it("resets input fields when reset button is clicked", async () => {
     // @ts-ignore
     render(App);
